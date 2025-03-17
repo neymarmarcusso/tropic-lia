62a657b7b7b748e6630c27ad946eaccc3c3e1b08
neymarmarcusso <162704187+neymarmarcusso@users.noreply.github.com>
2025-03-17 10:34:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded" , function(){
+document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById
     ('aumentar-fonte');
     const diminuirFonteBotao = document.getElementById
@@ -6,13 +6,23 @@ document.addEventListener("DOMContentLoaded" , function(){
 
     let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function() {
-       tamanhoAtualFonte += 0.1;
-       document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
 
-    diminuirFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
-})
+    aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+  })
+
+
+  diminuirFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+  })
+
+  const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+  const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+  botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+  })
+
+ })
